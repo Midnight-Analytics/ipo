162af07ef665f4a678727c704c6d69aef3609a37
Midnight-Analytics <tobias.finlay@midnight-analytics.com>
2021-04-11 01:58:19 +0100

    Removed Test Files

--- frontend/microblog.py
+++ frontend/microblog.py
@@ -2,5 +2,8 @@ from app import app
 import os
 
 
+# Run this command to run the applciation in DEBUG mode
+# docker run -p 5000:5000 -e DEBUG=1 <image-name>  e.g. flask_app_dev
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,  debug=os.environ.get('DEBUG') == '1')
